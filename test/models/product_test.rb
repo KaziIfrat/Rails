6f90d10cbc1b89691a42c60ab97c47b52984dcf0
Kazi Ifrat <ananyakazi92@gmail.com>
2018-10-07 14:37:41 -0400

    Fixture issues

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,17 +4,43 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   #
+         fixtures :products
+
+  test "product attributes must not be empty - i18n" do
+
+             product = products(:two)
+   assert product.invalid?
+
+
+
+
+
+ assert product.errors[:title].any? ,  product.errors[:title]
+
 =begin
-  test "product attributes must not be empty" do
-    product = Product.new
-    assert product.invalid?
-    assert product.errors[:title].any?
-    assert product.errors[:description].any?
-    assert product.errors[:price].any?
-    assert product.errors[:image_url].any?
+
+=end
+=begin
+
+=end
+=begin
+  assert product.errors[:description].any?
+=end
+=begin
+
+=end
+=begin
+
+=end
+=begin
+  assert product.errors[:price].any?
+
+
+
+  assert product.errors[:image_url].any?
+=end
   end
-
-
+=begin
   test "product price must be positive" do
     product = Product.new(title:
                               "My Book Title",
@@ -22,15 +48,64 @@ class ProductTest < ActiveSupport::TestCase
                           image_url:
                               "zzz.jpg")
     product.price = -1
-    assert product.invalid?
+    assert product.invalid? , "product is invalid"
     assert_equal ["must be greater than or equal to 0.01"],
                  product.errors[:price]
+
     product.price = 0
     assert product.invalid?
     assert_equal ["must be greater than or equal to 0.01"],
                  product.errors[:price]
     product.price = 1
-   # assert product.valid?
+   assert product.valid?
+
   end
 =end
-end
+
+=begin
+  test "product title must be unique" do
+    product = Product.new(title:
+                              "My Book Title",
+                          description: "yyy",
+                          image_url:
+                              "zzz.jpg")
+    product2 = Product.new(title:
+                              "My Book Title",
+                          description: "yyy",
+                          image_url:
+                              "zzz.jpg")
+    assert product.invalid?
+
+
+   assert_equal ["must be unique"],
+                 product.errors[:title]
+
+
+
+    end
+
+=end
+=begin
+     def new_product(image_url)
+     Product.new(title: "My Book Title",
+                 description: "yyy",
+                  price: 3  ,
+                 image_url: image_url)
+     end
+
+  test "image check" do
+  good=%w{fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+           http://a.b.c/x/y/z/fred.gif}
+      bad = %w{fred.doc fred.gif/more fred.gif.more}
+
+  good.each do |url|
+                assert new_product(url).valid? "#{url} is valid"
+  end
+
+   bad.each do |url|
+                 assert new_product(url).valid? , "#{url} is not valid"
+   end
+
+  end
+=end
+  end
