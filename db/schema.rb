96e433d14f6daec5b6fdba95446e4f2df913579b
Kazi Ifrat <ananyakazi92@gmail.com>
2018-10-14 11:21:25 -0400

    limiting access.Test helper

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_11_135939) do
+ActiveRecord::Schema.define(version: 2018_10_12_193632) do
 
   create_table "carts", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -44,6 +44,13 @@ ActiveRecord::Schema.define(version: 2018_10_11_135939) do
     t.text "description"
     t.string "image_url"
     t.decimal "price", precision: 8, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
